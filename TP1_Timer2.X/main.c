ce4f03e0ffde35a2c32cc332b5ed87a39b4c1b7a
VerebelyPaulloup <verebelyi.polo@gmail.com>
2026-01-20 15:07:32 +0100

    TP2a_PWM

--- TP1_Timer2.X/main.c
+++ TP1_Timer2.X/main.c
@@ -1,21 +1,3 @@
-/*
- * File:   main.c
- * Author: apirog
- *
- * Created on January 5, 2026, 12:08 PM
- */
-
-// Allume les LEDs D5-8 de la carte Microchip Explorer 8 
-// MCU : PIC16F1719 
-//
-// | LED | Pin |
-// |-----|-----|
-// |  D5 | RB0 |
-// |  D6 | RB1 |
-// |  D7 | RB2 |
-// |  D9 | RB3 |
-//
-
 #include <xc.h>
 #include "../Ressources/configbits.h"
 
---
+++ TP2a_pwm.X/main.c
@@ -0,0 +1,55 @@
+#include <xc.h>
+#include "../Ressources/configbits.h"
+
+void initPPS(){
+    PPSLOCK = 0x55;
+    PPSLOCK = 0xAA;
+    PPSLOCKbits.PPSLOCKED = 0; 
+    
+    TRISDbits.TRISD1 = 0;
+    RD0PPS = 15;
+    PPSLOCKbits.PPSLOCKED = 1;
+}
+
+void initPWML(){
+    PWM4DCH = 12;
+    PWM4DCL = 0;
+    
+    PWM4CONbits.PWM4EN = 1;
+}
+
+void initPWMH(){
+    PWM4DCH = 120;
+    PWM4DCL = 0;
+    
+    PWM4CONbits.PWM4EN = 1;
+}
+
+void initTimer(){
+    PR2 = 0b01111000;
+    T2CON = 0b00000110;
+}
+
+void initComponents(){        
+    TRISAbits.TRISA5 = 1;  
+    ANSELAbits.ANSA5 = 0;  
+   
+    TRISDbits.TRISD0 = 0;  
+  
+}
+
+void main(void) {
+    LATD &= 0x00;
+    LATB &= 0x00;
+    initComponents();
+    initPWML();
+    initTimer();
+    initPPS();
+    while(1) {
+        if (PORTAbits.RA5 == 0) { 
+            initPWMH();
+        } else {
+            initPWML();
+        }
+    }
+}
